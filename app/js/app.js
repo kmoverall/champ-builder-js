d27c5d0deff274948814854ec6a3f20578574799
Kevin Overall <kmoverall@gmail.com>
2014-05-15 13:36:33 -0400

    Nearly finished conversion to jQuery

--- app/js/app.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by kevinoverall on 4/17/14.
- */
-var app = angular.module('ChampBuilderApp',['ui.bootstrap']);
--- app/js/controllers/tabmenu.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by kevinoverall on 4/24/14.
- */
-var TabMenuCtrl = function ($scope) {
-    $scope.navType = 'pills';
-};
